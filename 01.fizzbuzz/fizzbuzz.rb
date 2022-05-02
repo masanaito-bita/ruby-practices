eba836f190029438134e8a2247df3d0661a85be7
masanaito-bita <masana_ito@bita.jp>
2022-05-02 13:50:47 +0900

    zeroメソッドの記述を修正、出力メソッド名を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -7,15 +7,15 @@ max_number = 20
 (1..max_number).each do |number|
   # @param [Integer] number
   def fizz_buzz(number)
-    if (number % 3).zero && (number % 5).zero
+    if (number % 3).zero? && (number % 5).zero?
       puts 'FizzBuzz'
-    elsif (number % 3).zero
+    elsif (number % 3).zero?
       puts 'Fizz'
-    elsif (number % 5).zero
+    elsif (number % 5).zero?
       puts 'Buzz'
     else
       puts number
     end
   end
-  puts fizzBuzz(number)
+  puts fizz_buzz(number)
 end
