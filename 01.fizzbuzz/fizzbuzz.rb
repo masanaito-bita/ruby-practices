40b6fbd96356126a42e89bf311ed5c189a391464
masanaito-bita <masana_ito@bita.jp>
2022-05-02 13:37:36 +0900

    rubocop実行しアラート部分を修正
    zeroメソッドで返り値を判定する
    空行を削除
    文字列をシングルクォートで囲う

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 # @type [Integer]
 max_number = 20
 
 # @type [Integer] number
 (1..max_number).each do |number|
-
   # @param [Integer] number
   def fizz_buzz(number)
-    if (number % 3) == 0 && (number % 5) == 0
-      puts "FizzBuzz"
-    elsif number % 3 == 0
-      puts "Fizz"
-    elsif number % 5 == 0
-      puts "Buzz"
+    if (number % 3).zero && (number % 5).zero
+      puts 'FizzBuzz'
+    elsif (number % 3).zero
+      puts 'Fizz'
+    elsif (number % 5).zero
+      puts 'Buzz'
     else
       puts number
     end
