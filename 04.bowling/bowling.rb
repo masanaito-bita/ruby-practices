4d5fa8558bd6215372ed2f97dc7e0d46ddff0c69
masanaito-bita <masana_ito@bita.jp>
2022-05-15 01:05:24 +0900

    三項演算子に修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -21,11 +21,7 @@ frames.each_with_index do |frame, i|
   next unless frame.sum == 10 && i < 9
 
     if frame[0] == 10
-      if frames[i + 1][0] == 10
-        point += frames[i + 1][0] + frames[i + 2][0]
-      else
-        point += frames[i + 1].sum
-      end
+      point += frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
     else
       point += frames[i + 1][0]
     end
