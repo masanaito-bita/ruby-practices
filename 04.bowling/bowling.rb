d9538b72c31d6844c15ff72eef796c741a636206
masanaito-bita <masana_ito@bita.jp>
2022-05-19 11:16:42 +0900

    リファクタリング
    flat_mapメソッドを使用し標準入力を受け取り配列にする
    1フレームごとに配列を分ける処理をシンプルにする

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,30 +1,21 @@
 # frozen_string_literal: true
 
-shots = []
-ARGV[0].split(',').each do |score|
-  if score == 'X'
-    shots << 10
-    shots << 0
-  else
-    shots << score.to_i
-  end
+shots = ARGV[0].split(",").flat_map do |score|
+  score == "X" ? [10, 0] : score.to_i
 end
 
-frames = []
-shots.each_slice(2) do |score|
-  frames << score
-end
+frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, i|
   point += frame.sum
   next unless frame.sum == 10 && i < 9
 
-    if frame[0] == 10
-      point += frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
-    else
-      point += frames[i + 1][0]
-    end
+  if frame[0] == 10
+    point += frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
+  else
+    point += frames[i + 1][0]
+  end
 end
 
 puts point
