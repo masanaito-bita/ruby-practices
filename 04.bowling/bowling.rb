935a078441c0b4181f2e9a38bc4905baaf63331e
masanaito-bita <masana_ito@bita.jp>
2022-05-22 11:46:48 +0900

    each_with_indexにsumメソッドを利用

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -6,17 +6,15 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = 0
-frames.each_with_index do |frame, i|
-  point += frame.sum
-  next unless frame.sum == 10 && i < 9
+point = frames.each_with_index.sum do |frame, i|
+  next frame.sum unless frame.sum == 10 && i < 9
 
   next_frame = frames[i + 1]
   after_next_frame = frames[i + 2]
   if frame[0] == 10
-    point += next_frame[0] == 10 ? next_frame[0] + after_next_frame[0] : next_frame.sum
+    next_frame[0] == 10 ? frame.sum + next_frame[0] + after_next_frame[0] : frame.sum + next_frame.sum
   else
-    point += next_frame[0]
+    frame.sum + next_frame[0]
   end
 end
 
